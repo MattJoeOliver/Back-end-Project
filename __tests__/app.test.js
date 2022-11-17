2aaa3e7ed961595b5352e4d8ca37931a7ed2013f
Matt <109768190+MattJoeOliver@users.noreply.github.com>
2022-11-17 10:56:43 +0000

    additional tests on GET /api/categories and /api/reviews endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,6 +20,7 @@ describe("GET /api/categories", () => {
       .then(({ body }) => {
         const { categories } = body;
         expect(categories).toBeInstanceOf(Array);
+        expect(categories.length).toBeGreaterThan(0)
         categories.forEach((category) => {
           expect(category).toMatchObject({
             slug: expect.any(String),
@@ -28,37 +29,56 @@ describe("GET /api/categories", () => {
         });
       });
   });
+  test("status:404, invalid path (misspelling)", () => {
+    return request(app)
+      .get("/api/categorys")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Path not found");
+      });
+  });
 });
 
 describe("GET /api/reviews", () => {
-    test('status:200, responds with an array of review objects', () => {
-        return request(app)
-        .get("/api/reviews")
-        .expect(200)
-        .then(({ body }) => {
-            const { reviews } = body
-            expect(reviews).toBeInstanceOf(Array);
-            reviews.forEach((review) => {
-                expect(review).toMatchObject({
-                    owner: expect.any(String),
-                    title: expect.any(String),
-                    review_id: expect.any(Number),
-                    category: expect.any(String),
-                    review_img_url: expect.any(String),
-                    created_at: expect.any(String),
-                    votes: expect.any(Number),
-                    designer: expect.any(String),
-                    comment_count: expect.any(String)
-                })
-            })
-        })
-    })
-    test('status:200, respond with an array sorted by date in descending order', () => {
-        return request(app)
-        .get("/api/reviews")
-        .expect(200)
-        .then(res => {
-            expect(res.body.reviews).toBeSortedBy('created_at', { descending: true })
-        })
-    })
-})
+  test("status:200, responds with an array of review objects", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        const { reviews } = body;
+        expect(reviews).toBeInstanceOf(Array);
+        expect(reviews.length).toBeGreaterThan(0)
+        reviews.forEach((review) => {
+          expect(review).toMatchObject({
+            owner: expect.any(String),
+            title: expect.any(String),
+            review_id: expect.any(Number),
+            category: expect.any(String),
+            review_img_url: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            designer: expect.any(String),
+            comment_count: expect.any(String),
+          });
+        });
+      });
+  });
+  test("status:200, respond with an array sorted by date in descending order", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.reviews).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
+  });
+  test("status:404, invalid path (misspelling)", () => {
+    return request(app)
+      .get("/api/revieus")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Path not found");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -8,4 +8,8 @@ app.get("/api/categories", getCategories);
 
 app.get("/api/reviews", getReviews);
 
+app.all('/*', (req, res, next) => {
+    res.status(404).send({msg: 'Path not found'})
+})
+
 module.exports = app;
